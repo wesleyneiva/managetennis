2069f6126404042337d897950235a891f4aaa35b
Wesley <wesleyneiva15@hotmail.com>
2025-10-01 17:01:16 -0300

    add novo app firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@ export const environment = {
         projectId: "mygame-9e93d",
         storageBucket: "mygame-9e93d.firebasestorage.app",
         messagingSenderId: "295393862585",
-        appId: "1:295393862585:web:807cc1c8466fb654464c68"
+        appId: "1:295393862585:web:b8bbd882d831748d464c68"
     }
 };
